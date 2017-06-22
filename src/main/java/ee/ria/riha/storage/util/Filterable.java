1c4d80ff660ca98607e52c8f294cdb4d7825b5aa
Valentin Suhnjov <valentis.s@gmail.com>
2017-06-22 10:45:28 +0300

    RIHAKB-149. Merge RIHA-Browser argument resolvers

---
+++ src/main/java/ee/ria/riha/storage/util/Filterable.java
@@ -0,0 +1,14 @@
+package ee.ria.riha.storage.util;
+
+/**
+ * @author Valentin Suhnjov
+ */
+public interface Filterable {
+
+    String getFilter();
+
+    String getSort();
+
+    String getFields();
+
+}
